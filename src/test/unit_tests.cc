931eb326bcd427f6c3188dbc1cd8e6887a457903
Joann Nuts <joannnut@pr-p4.kzn.21-school.ru>
2023-08-19 16:04:45 +0300

    tests_2

--- src/test/unit_tests.cc
+++ src/test/unit_tests.cc
@@ -15,6 +15,27 @@ TEST(ViewerTest, Test_2) {
   std::string file_name = "cube.obj";
   s21::Model model;
   model.ReadDataFile(file_name);
+  EXPECT_EQ(model.GetEdgesCount(), 10);
+  EXPECT_EQ(model.GetVertexCount(), 24);
+  EXPECT_EQ(model.GetFacetsCount(), 60);
+  // std::cout << "VERTEX = " << model.GetVertexes()[model.GetEdgesCount()]
+  //           << "\n";
+  EXPECT_NEAR(model.GetVertexes()[model.GetEdgesCount()], 2, 1e-5);
+  EXPECT_EQ(model.GetFacets()[model.GetEdgesCount()], 6);
+
+  file_name = "lamp.obj";
+  model.ReadDataFile(file_name);
+  EXPECT_EQ(model.GetEdgesCount(), 4403);
+  EXPECT_EQ(model.GetVertexCount(), 13320);
+  EXPECT_EQ(model.GetFacetsCount(), 35262);
+  EXPECT_NEAR(model.GetVertexes()[model.GetEdgesCount()], -1.42317, 1e-5);
+  EXPECT_EQ(model.GetFacets()[model.GetEdgesCount()], 1680);
+};
+
+TEST(ViewerTest, Test_3) {
+  std::string file_name = "cube.obj";
+  s21::Model model;
+  model.ReadDataFile(file_name);
   model.ChangeModel("rotate", 0.2, 1);
   EXPECT_EQ(model.GetEdgesCount(), 10);
   EXPECT_EQ(model.GetVertexCount(), 24);
@@ -28,7 +49,7 @@ TEST(ViewerTest, Test_2) {
   EXPECT_EQ(model.GetEdgesCount(), 4403);
   EXPECT_EQ(model.GetVertexCount(), 13320);
   EXPECT_EQ(model.GetFacetsCount(), 35262);
-  EXPECT_NEAR(model.GetVertexes()[model.GetEdgesCount()], -1.42317, 5);
+  EXPECT_NEAR(model.GetVertexes()[model.GetEdgesCount()], -1.42317, 1e-5);
   EXPECT_EQ(model.GetFacets()[model.GetEdgesCount()], 1680);
 
   file_name = "female.obj";
@@ -37,19 +58,8 @@ TEST(ViewerTest, Test_2) {
   EXPECT_EQ(model.GetEdgesCount(), 124120);
   EXPECT_EQ(model.GetVertexCount(), 187014);
   EXPECT_EQ(model.GetFacetsCount(), 744720);
-  EXPECT_NEAR(model.GetVertexes()[model.GetEdgesCount()], 6.93511, 5);
+  EXPECT_NEAR(model.GetVertexes()[model.GetEdgesCount()], 6.93511, 1e-5);
   EXPECT_EQ(model.GetFacets()[model.GetEdgesCount()], 13838);
-};
-
-TEST(ViewerTest, Test_3) {
-  std::string file_name = "cube.obj";
-  s21::Model model;
-  model.ChangeModel("rotate", 0.2, 2);
-  EXPECT_EQ(model.GetEdgesCount(), 10);
-  EXPECT_EQ(model.GetVertexCount(), 24);
-  EXPECT_EQ(model.GetFacetsCount(), 60);
-  EXPECT_NEAR(model.GetVertexes()[model.GetEdgesCount()], 2, 5);
-  EXPECT_EQ(model.GetFacets()[model.GetEdgesCount()], 6);
 };
 
 int main(int argc, char** argv) {
